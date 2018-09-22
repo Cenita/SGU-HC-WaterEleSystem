fe7cae305f86a44be75a2b2924db3d3e63e010b7
Cenita <411170425@qq.com>
2018-09-23 00:02:20 +0800

    修复横向宽度bug

--- js/EasterEgg.js
+++ js/EasterEgg.js
@@ -42,7 +42,7 @@ $(
           $("#logo1").animate({left:"55px",width:"40px",top:"-4px",left:"47px"}).rotate({animateTo:61});
           $("#logo2").animate({opacity:"1",top:"-5px",left:"-25px",width:"32px",height:"41px"});
           $("#mask").show().delay(600).animate({left:"310px"},1000);
-          $("#logoEnd").show().delay(600).animate({opacity:"1"});
+          $("#logoEnd").show().delay(600).animate({opacity:"1"}).delay(1200).hide();
           $(".logoPart").css("cursor","pointer");
           $(".logoPart").css("height","50px");
         }
