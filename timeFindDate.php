14b1d907ec173330f783f5b2fd793754af321549
Cenita <411170425@qq.com>
2018-09-23 13:43:12 +0800

    水电费位置互换

--- timeFindDate.php
+++ timeFindDate.php
@@ -11,26 +11,6 @@ $endDate=date("Y-m-d");
 <link rel="stylesheet" href="css/timefind.css">
 <link rel="stylesheet" href="js/kinerDatePicker/css/index.css">
 <div class="ui raised segment" style="height: 100%!important;">
-    <div class="ui horizontal divider header">水费定向查询</div>
-    <div class="findPart">
-        <div id="waterStartDate" class="dateItem"  startYear="2015" title="请选择开始时间" default-val="<?php echo $startDate?>">开始时间</div>
-        <div style="width:1%;">-</div>
-        <div id="waterEndDate" class="dateItem"  startYear="2015" title="请选择结束时间" default-val="<?php echo $endDate?>">结束时间</div>
-    </div>
-    <i class="fa fa-search"id="waterSearch" style="width: 2%;"></i>
-    <div id="waterSearchPart" class="searchPart">
-        <div class="ui middle aligned list weList" style="display: none;">
-            <div class="item">
-                <div>金额</div>
-                <div>日期</div>
-            </div>
-        </div>
-        <div class="ui loading segment" style="display: none;height:200px;margin-top: 0px;">
-        </div>
-    </div>
-    <div id="waterTips" style="display: none">(最多显示十五条)</div>
-</div>
-<div class="ui raised segment" style="height: 100%!important;">
     <h4 class="ui horizontal divider header">电费定向查询</h4>
     <div class="findPart">
         <div id="eleStartDate" class="dateItem"  startYear="2015" title="请选择开始时间" default-val="<?php echo $startDate?>">开始时间</div>
@@ -49,4 +29,24 @@ $endDate=date("Y-m-d");
         </div>
     </div>
     <div id="eleTips" style="display: none">(最多显示十五条)</div>
+</div>
+<div class="ui raised segment" style="height: 100%!important;">
+    <div class="ui horizontal divider header">水费定向查询</div>
+    <div class="findPart">
+        <div id="waterStartDate" class="dateItem"  startYear="2015" title="请选择开始时间" default-val="<?php echo $startDate?>">开始时间</div>
+        <div style="width:1%;">-</div>
+        <div id="waterEndDate" class="dateItem"  startYear="2015" title="请选择结束时间" default-val="<?php echo $endDate?>">结束时间</div>
+    </div>
+    <i class="fa fa-search"id="waterSearch" style="width: 2%;"></i>
+    <div id="waterSearchPart" class="searchPart">
+        <div class="ui middle aligned list weList" style="display: none;">
+            <div class="item">
+                <div>金额</div>
+                <div>日期</div>
+            </div>
+        </div>
+        <div class="ui loading segment" style="display: none;height:200px;margin-top: 0px;">
+        </div>
+    </div>
+    <div id="waterTips" style="display: none">(最多显示十五条)</div>
 </div>
